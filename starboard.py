6cd1a3d70cd904dbc67121f787d85a769789677a
Gerard Bentley <gerardrbentley@gmail.com>
2022-03-21 13:21:44 -0700

    feat: show code

--- starboard.py
+++ starboard.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-starboard_embed_url = st.text_area('Starboard.gg iframe', value="https://ifusernameisnone.starboard.host/v1/embed/0.15.3/c3blnrq23akg00819700/nxIEOXJ/")
+with st.echo():
+    starboard_embed_url = st.text_area('Starboard.gg iframe', value="https://ifusernameisnone.starboard.host/v1/embed/0.15.3/c3blnrq23akg00819700/nxIEOXJ/")
 
-iframe_template = """<iframe src="{}" frameborder="0" height="600px" width="100%"></iframe>"""
-st.write(starboard_embed_url)
-st.markdown(iframe_template.format(starboard_embed_url), unsafe_allow_html=True)
+    iframe_template = """<iframe src="{}" frameborder="0" height="600px" width="100%"></iframe>"""
+    st.write(starboard_embed_url)
+    st.markdown(iframe_template.format(starboard_embed_url), unsafe_allow_html=True)
